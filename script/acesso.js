47e1125a53b4def7c59f891cd4b9376c8f95268c
Jackson Almeida <jacksonadh@gmail.com>
2020-12-04 12:48:42 -0300

    Erro
    js não redireciona após login.

--- script/acesso.js
+++ script/acesso.js
@@ -1,24 +1,32 @@
 $(function(){
-    $("button#btn-entrar").on("click", function(e){
+      $("button#btn-entrar").on("click", function(e){
         e.preventDefault();
-        var campoEmail = $("form#formulario-login #email").val();
-        var campoSenha = $("form#formulario-login #senha").val();
-        $.ajax({
-            url: "acoes/login.php",
-            type: "POST",
-            data: {
-                inputEmail: campoEmail,
-                inputPassword: campoSenha
-            },
-            sucess: function(retorno){
-                retorno = JSON.parse(retorno);
-                if(retorno["erro"]){
-                    $("div#mensagem").html(retorno["mensagem"]);
-                }else{
-                    window.location = "dashboard.php"
+        var campoEmail = $("form#formulario-login #inputEmail").val();
+        var campoSenha = $("form#formulario-login #inputPassword").val();
+        if(campoEmail.trim() == "" || campoSenha.trim() == ""){
+            $("div#mensagem").html("Preencha todos os campos.");
+        }else{
+            $.ajax({
+                url: "acoes/login.php",
+                type: "POST",
+                data: {
+                    email: campoEmail,
+                    senha: campoSenha
+                },
+                success: function(retorno){
+                    retorno = JSON.parse(retorno);
+
+                    if(retorno["erro"]){
+                        $("div#mensagem").html(retorno["mensagem"]);
+                    }else{
+                        window.location = "dashboard.php";
+                    }           
+                },
+                error: function(){
+                    $("div#mensagem").html(retorno["Ocorreu um erro durante a solicitação"]);
                 }
-               
-            }
-        })
-    })
+            });
+        }
+     });
+    
 });
